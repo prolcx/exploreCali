a1d302b0b0008c6cfccfcacd1366378e3ae00095
davidcsl <singaporecsl123@gmail.com>
2022-05-26 21:13:29 +0800

    Fourth

--- src/main/java/com/example/ec/repo/TourRatingRespository.java
+++ src/main/java/com/example/ec/repo/TourRatingRespository.java
@@ -2,7 +2,7 @@ package com.example.ec.repo;
 
 import com.example.ec.domain.Tour;
 import com.example.ec.domain.TourRating;
-import com.example.ec.domain.TourRatingPk;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
